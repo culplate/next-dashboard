eaa9d71a0e74b1911814dcc90aba6ee8edf8cfc9
culplate <culplate@gmail.com>
2024-11-16 15:46:07 +0100

    StatusLabel: added conditional classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled={true}>
+        Suspended
+      </StatusLabel>
     </main>
   );
 }
