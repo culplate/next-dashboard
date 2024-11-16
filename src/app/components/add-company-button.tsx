ab71fa404f149ea2624370c6f589c71876ae3ea3
culplate <culplate@gmail.com>
2024-11-16 20:21:48 +0100

    Created: add company modal with components

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, { useState } from 'react';
+import Button from '@/app/components/button';
+import dynamic from 'next/dynamic';
+// import CompanyFormModal from './company-form-modal';
+
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add Company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => {
+          setShow(false);
+        }}
+      />
+    </>
+  );
+}
