a7a0c61e2dd6fc873e72a83fd0d8cef2e42dd9c7
culplate <culplate@gmail.com>
2024-11-16 20:22:35 +0100

    code cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import StatusLabel, { Status } from './components/statusLabel/StatusLabel';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended} disabled={true}>
-        Suspended
-      </StatusLabel>
     </main>
   );
 }
